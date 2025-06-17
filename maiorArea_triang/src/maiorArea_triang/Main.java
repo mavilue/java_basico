bfbf2f3bea3460c1333252f0792a4b6f91f45bc4
mavi <150869512+mavilue@users.noreply.github.com>
2025-06-16 21:30:37 -0300

    Add files via upload

---
+++ maiorArea_triang/src/maiorArea_triang/Main.java
@@ -0,0 +1,47 @@
+package maiorArea_triang;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		System.out.println("Enter the measures of tringule X: ");
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		double c = sc.nextDouble();
+		
+		System.out.println("Enter the measures of tringule Y: ");
+		double d = sc.nextDouble();
+		double e = sc.nextDouble();
+		double f = sc.nextDouble();
+
+		double areaX = calculoValor(a,b,c);
+		double areaY = calculoValor(d,e,f);
+		
+		resultadoPrt(areaX,areaY);
+		
+		sc.close();
+	}
+	
+	public static double calculoValor(double a, double b, double c) {
+		double p = (a+b+c)/2;
+		return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+	}
+	
+	public static void resultadoPrt(double areaX, double areaY) {
+		System.out.println("Triangule X area: " + String.format("%.4f",areaX));
+		System.out.println("Triangule Y area: " + String.format("%.4f",areaY));
+		
+		if (areaX > areaY) {
+			System.out.println("Larger area = X");
+		}
+		else {
+			System.out.println("Larger area = Y");
+		}
+	}
+
+}
---
+++ maiorArea_triang/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module maiorArea_triang {
+}
